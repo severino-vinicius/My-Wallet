111423b00ee364fa8dbc8d08eac0dddeaaf5d835
Severino Vinicius <severino.sales95@gmail.com>
2023-05-15 17:46:31 -0300

    Requisito 06 - Finalizado e testado

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    // const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <>
+        <div>Table</div>
+        <tr>
+          <th> Descrição </th>
+          <th> Tag </th>
+          <th> Método de pagamento </th>
+          <th> Valor </th>
+          <th> Moeda </th>
+          <th> Câmbio utilizado </th>
+          <th> Valor convertido </th>
+          <th> Moeda de conversão </th>
+          <th> Editar/Excluir </th>
+          {/* { expenses.map(({ id, value, description, currency, method, tag }) => (
+            <th>
+              {`${description}`}
+            </th>
+            <th>
+              {`${tag}`}
+            </th>
+            <th>
+              {`${method}`}
+            </th>
+            <th>
+              {`${value}`}
+            </th>
+          )) } */}
+        </tr>
+      </>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -11,6 +12,8 @@ class Wallet extends React.Component {
           Wallet
         </div>
         <WalletForm />
+        <hr />
+        <Table />
       </>
     );
   }
